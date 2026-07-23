46b376ecc5c3d4bb055412614fd2838355a1943f
akio_utsumoto <akio.utsumoto@terra-drone.co.jp>
2026-07-23 09:02:33 +0900

    release df80c77a:gaba1

--- licenses.js
+++ licenses.js
@@ -1,7 +1,6 @@
 // Terra floating-license registry.
 // Managed automatically by terra-license.lsp. Do not edit by hand.
 var TL_LICENSES = [
-"df80c77a:gaba1:2461245.367007",
 "df80c77a:gaba2:2461245.367093",
 "df80c77a:gaba3:2461245.367199",
 ];
