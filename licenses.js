f80327920b23196cd8f2d7739d0d648dff5fbd84
akio_utsumoto <akio.utsumoto@terra-drone.co.jp>
2026-08-19 13:19:40 +0900

    acquire 4a580a20:aaaa

--- licenses.js
+++ licenses.js
@@ -2,5 +2,5 @@
 // Managed automatically by license.lsp. Do not edit by hand.
 var TL_LICENSES = [
 "df80c77a:gaba:2461250.406412:1286724327",
-"4a580a20:aaaa:2461272.548889:562872857",
+"4a580a20:aaaa:2461272.555324:562872857",
 ];
